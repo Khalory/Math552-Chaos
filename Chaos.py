3ea9fd8e3950d6c57d4a246f0affc6869b9b1ab1
Khalory <tk100794@gmail.com>
2017-04-26 01:57:42 -0400

    ?

--- Chaos.py
+++ Chaos.py
@@ -4,18 +4,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
-fig, ax = plt.subplots(2)
+fig, ax = plt.subplots(3)
 
-n = 256
+n = 128
 t = np.arange(n)
 line, = ax[0].plot(t, np.zeros(n))
 line2, = ax[1].plot(t, np.zeros(n))
 lines = [line, line2]
-ax[0].set_ylim([0,1])
-ax[1].set_xlim([0,1])
-ax[1].set_ylim([-.25,1])
+ax[0].set_ylim([0, 1])
+ax[1].set_xlim([0, 1])
+ax[1].set_ylim([-.25, 1])
+ax[2].set_xlim([0, 4])
+ax[2].set_ylim([0, 1])
 
-def getLogisticMap(r):
+def getLogisticMap(r, n):
 	y = np.zeros(n)
 	y[0] = .5
 	for i in np.arange(n):
@@ -23,8 +25,14 @@ def getLogisticMap(r):
 			y[i] = r * y[i-1] * (1 - y[i-1])
 	return y
 
+def bifurcation():
+	for r in np.arange(0, 4, 4/1000):
+		ys = getLogisticMap(r, 200)
+		ys = ys[100:]
+		ax[2].scatter([r] * len(ys), ys, s=[.5]*len(ys), marker='.', color='blue')
+
 def animate(i):
-	m = getLogisticMap(i)
+	m = getLogisticMap(i, n)
 	lines[0].set_ydata(m)  # update the data
 	fourier = np.fft.fft(m)
 	xf = np.linspace(0.0, 1.0, n)
@@ -39,6 +47,7 @@ def init():
     lines[1].set_ydata(np.ma.array(t, mask=True))
     return lines
 
+bifurcation()
 ani = animation.FuncAnimation(fig, animate, np.arange(1, 5, .0125), init_func=init,
                               interval=100, blit=True)
 plt.show()
